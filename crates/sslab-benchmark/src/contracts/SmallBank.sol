a8b559d12b503390f3feb49543296a4203bbd8e4
Donghyeon Ryu <ydh9516@gmail.com>
2023-09-19 10:33:40 +0900

    contract binary code should be 'deployedBinary' when directly imports it from the source code.

--- crates/sslab-benchmark/src/contracts/SmallBank.sol
+++ crates/sslab-benchmark/src/contracts/SmallBank.sol
@@ -46,7 +46,6 @@ contract SmallBank {
         uint bal2 = checkingStore[arg1];
         uint amount = arg2;
         
-        require(bal1 >= amount);
         bal1 -= amount;
         bal2 += amount;
         

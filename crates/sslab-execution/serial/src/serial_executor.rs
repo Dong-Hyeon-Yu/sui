732f1f80764819e1d7acd3c90908982a0ee05016
DongHyeon Ryu <ydh9516@gmail.com>
2024-04-15 17:47:08 +0900

    reconfigure benchmarks of serial execution

--- crates/sslab-execution/serial/src/serial_executor.rs
+++ crates/sslab-execution/serial/src/serial_executor.rs
@@ -1,38 +1,32 @@
 use std::sync::Arc;
 
 use sslab_execution::{
-    executor::Executable, 
-    evm_storage::{SerialEVMStorage, backend::ExecutionBackend}, 
-    types::{ExecutableEthereumBatch, ExecutionResult}
+    evm_storage::{backend::ExecutionBackend, ConcurrentEVMStorage},
+    executor::Executable,
+    types::{ExecutableEthereumBatch, ExecutionResult},
 };
-use tracing::{warn, info, trace};
-
+use tracing::{info, trace, warn};
 
 #[async_trait::async_trait]
 impl Executable for SerialExecutor {
     async fn execute(&self, consensus_output: Vec<ExecutableEthereumBatch>) {
-
         for batch in consensus_output {
             let _ = self._execute(batch);
         }
     }
 }
 
-
 pub struct SerialExecutor {
-    global_state: Arc<SerialEVMStorage>,
+    global_state: Arc<ConcurrentEVMStorage>,
 }
 
 impl SerialExecutor {
-    pub fn new(global_state: Arc<SerialEVMStorage>) -> Self {
+    pub fn new(global_state: Arc<ConcurrentEVMStorage>) -> Self {
         info!("Execution mode: 'serial'");
-        Self {
-            global_state
-        }
+        Self { global_state }
     }
 
     pub fn _execute(&self, batch: ExecutableEthereumBatch) -> ExecutionResult {
-
         let state = self.global_state.clone();
 
         let digest = batch.digest().clone();
@@ -40,17 +34,15 @@ impl SerialExecutor {
         std::thread::spawn(move || {
             for tx in batch.data() {
                 match crate::evm_utils::execute_tx(tx, state.as_ref()) {
-                    Ok(Some((effect, _))) 
-                        => state.apply_local_effect(effect),
-                    Ok(None) 
-                        => trace!("{:?} may be reverted.", tx.id()),
-                    Err(e) 
-                        => warn!("fail to execute a transaction {:?}", e)
+                    Ok(Some((effect, _))) => state.apply_local_effect(effect),
+                    Ok(None) => trace!("{:?} may be reverted.", tx.digest_u64()),
+                    Err(e) => warn!("fail to execute a transaction {:?}", e),
                 }
             }
-        }).join().expect("fail to execute transactions");
-        
+        })
+        .join()
+        .expect("fail to execute transactions");
+
         ExecutionResult::new(vec![digest])
     }
 }
-

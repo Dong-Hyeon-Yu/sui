998b2600b0a962dbe08967767363444895bdab61
DongHyeon Ryu <ydh9516@gmail.com>
2024-04-18 21:15:39 +0900

    fill benchmark txs

--- crates/sslab-execution/evm/src/utils/test_utils.rs
+++ crates/sslab-execution/evm/src/utils/test_utils.rs
@@ -1,21 +1,25 @@
-use std::{sync::Arc, str::FromStr};
+use self::small_bank::SmallBank;
+use crate::types::{EthereumTransaction, ExecutableEthereumBatch};
 use bytes::Bytes;
-use ethers_core::{types::{Signature, U256, transaction::eip2718::TypedTransaction, H160}, rand};
-use ethers_providers::{Provider, MockProvider};
+use ethers_core::{
+    rand,
+    types::{transaction::eip2718::TypedTransaction, Signature, H160, U256},
+};
+use ethers_providers::{MockProvider, Provider};
 use ethers_signers::{LocalWallet, Signer};
 use narwhal_types::BatchDigest;
 use rand::Rng as _;
-use rand_distr::{Zipf, Uniform, Distribution};
+use rand_distr::{Distribution, Uniform, Zipf};
 use rayon::prelude::*;
-use crate::types::{EthereumTransaction, ExecutableEthereumBatch};
-use self::small_bank::SmallBank;
+use std::{str::FromStr, sync::Arc};
 
-
-pub const ADMIN_SECRET_KEY: &[u8] = &[95 as u8, 126, 251, 131, 73, 90, 235, 201, 21, 22, 203, 137, 149, 240, 205, 60, 221, 27, 81, 53, 2, 200, 90, 185, 25, 240, 166, 21, 177, 41, 49, 254];
+pub const ADMIN_SECRET_KEY: &[u8] = &[
+    95 as u8, 126, 251, 131, 73, 90, 235, 201, 21, 22, 203, 137, 149, 240, 205, 60, 221, 27, 81,
+    53, 2, 200, 90, 185, 25, 240, 166, 21, 177, 41, 49, 254,
+];
 // pub const ADMIN_ADDRESS: &str = "0xe14de1592b52481b94b99df4e9653654e14fffb6";
 pub const DEFAULT_CONTRACT_ADDRESS: &str = "0x1000000000000000000000000000000000000000";
-pub const DEFAULT_CHAIN_ID: u64 = 9;  // ISTANBUL 
-
+pub const DEFAULT_CHAIN_ID: u64 = 9; // ISTANBUL
 
 pub struct SmallBankTransactionHandler {
     admin_wallet: LocalWallet,
@@ -29,15 +33,26 @@ pub struct SmallBankTransactionHandler {
 impl SmallBankTransactionHandler {
     pub fn new(provider: Provider<MockProvider>, chain_id: u64) -> SmallBankTransactionHandler {
         SmallBankTransactionHandler {
-            admin_wallet: LocalWallet::from_bytes(ADMIN_SECRET_KEY.try_into().unwrap()).unwrap().with_chain_id(chain_id),
+            admin_wallet: LocalWallet::from_bytes(ADMIN_SECRET_KEY.try_into().unwrap())
+                .unwrap()
+                .with_chain_id(chain_id),
             chain_id,
-            contract: Some(SmallBank::new(H160::from_str(DEFAULT_CONTRACT_ADDRESS).unwrap(), Arc::new(provider))),
+            contract: Some(SmallBank::new(
+                H160::from_str(DEFAULT_CONTRACT_ADDRESS).unwrap(),
+                Arc::new(provider),
+            )),
             random_op_gen: Uniform::new(1, 7),
             val_gen: Uniform::new(1, 1000),
         }
     }
 
-    pub fn create_batches(&self, batch_size: usize, batch_num: usize, zipfian_coef: f32, account_num: u64) -> Vec<ExecutableEthereumBatch> {
+    pub fn create_batches(
+        &self,
+        batch_size: usize,
+        batch_num: usize,
+        zipfian_coef: f32,
+        account_num: u64,
+    ) -> Vec<ExecutableEthereumBatch> {
         let target_tnx_num = batch_size * batch_num;
 
         let mut buffer = (0..target_tnx_num)
@@ -45,17 +60,25 @@ impl SmallBankTransactionHandler {
             .map(|_| self.random_operation(zipfian_coef, account_num))
             .collect::<hashbrown::HashSet<_>>();
 
-        while buffer.len() == target_tnx_num {
+        while buffer.len() < target_tnx_num {
             buffer.insert(self.random_operation(zipfian_coef, account_num));
         }
 
-        buffer.into_par_iter().collect::<Vec<_>>()
+        buffer
+            .into_par_iter()
+            .collect::<Vec<_>>()
             .par_chunks_exact(batch_size)
             .map(|chunk| ExecutableEthereumBatch::new(chunk.to_vec(), BatchDigest::default()))
             .collect()
     }
 
-    pub fn create_raw_batches(&self, batch_size: usize, batch_num: usize, zipfian_coef: f32, account_num: u64) -> Vec<Vec<bytes::Bytes>> {
+    pub fn create_raw_batches(
+        &self,
+        batch_size: usize,
+        batch_num: usize,
+        zipfian_coef: f32,
+        account_num: u64,
+    ) -> Vec<Vec<bytes::Bytes>> {
         let target_tnx_num = batch_size * batch_num;
 
         let mut buffer = (0..target_tnx_num)
@@ -63,18 +86,19 @@ impl SmallBankTransactionHandler {
             .map(|_| self.random_operation_raw(zipfian_coef, account_num))
             .collect::<hashbrown::HashSet<_>>();
 
-        while buffer.len() == target_tnx_num {
+        while buffer.len() < target_tnx_num {
             buffer.insert(self.random_operation_raw(zipfian_coef, account_num));
         }
 
-        buffer.into_par_iter().collect::<Vec<_>>()
+        buffer
+            .into_par_iter()
+            .collect::<Vec<_>>()
             .par_chunks_exact(batch_size)
             .map(|chunk| chunk.to_vec())
             .collect::<Vec<_>>()
     }
 
     pub fn random_operation(&self, zipfian_coef: f32, account_num: u64) -> EthereumTransaction {
-
         let acc_gen = Zipf::new(account_num, zipfian_coef).unwrap();
         let acc1 = rand::thread_rng().sample(acc_gen).to_string();
         let acc2 = rand::thread_rng().sample(acc_gen).to_string();
@@ -96,7 +120,6 @@ impl SmallBankTransactionHandler {
     }
 
     pub fn random_operation_raw(&self, zipfian_coef: f32, account_num: u64) -> bytes::Bytes {
-
         let acc_gen = Zipf::new(account_num, zipfian_coef).unwrap();
         let acc1 = rand::thread_rng().sample(acc_gen).to_string();
         let acc2 = rand::thread_rng().sample(acc_gen).to_string();
@@ -123,7 +146,12 @@ impl SmallBankTransactionHandler {
     }
 
     fn create_account(&self, acc: String, init_check: U256, init_save: U256) -> TypedTransaction {
-        let mut tx = self.contract.as_ref().unwrap().create_account(acc, init_check, init_save).tx;
+        let mut tx = self
+            .contract
+            .as_ref()
+            .unwrap()
+            .create_account(acc, init_check, init_save)
+            .tx;
         self.set_default_params(&mut tx);
         tx
     }
@@ -141,24 +169,39 @@ impl SmallBankTransactionHandler {
     }
 
     fn send_payment(&self, from: String, to: String, amount: U256) -> TypedTransaction {
-        let mut tx = self.contract.as_ref().unwrap().send_payment(from, to, amount).tx;
+        let mut tx = self
+            .contract
+            .as_ref()
+            .unwrap()
+            .send_payment(from, to, amount)
+            .tx;
         self.set_default_params(&mut tx);
         tx
     }
 
     fn update_balance(&self, addr: String, amount: U256) -> TypedTransaction {
-        let mut tx = self.contract.as_ref().unwrap().deposit_checking(addr, amount).tx;
+        let mut tx = self
+            .contract
+            .as_ref()
+            .unwrap()
+            .deposit_checking(addr, amount)
+            .tx;
         self.set_default_params(&mut tx);
         tx
     }
 
     fn update_saving(&self, addr: String, amount: U256) -> TypedTransaction {
-        let mut tx = self.contract.as_ref().unwrap().update_saving(addr, amount).tx;
+        let mut tx = self
+            .contract
+            .as_ref()
+            .unwrap()
+            .update_saving(addr, amount)
+            .tx;
         self.set_default_params(&mut tx);
         tx
     }
 
-    fn write_check(&self, addr: String, amount: U256) -> TypedTransaction  {
+    fn write_check(&self, addr: String, amount: U256) -> TypedTransaction {
         let mut tx = self.contract.as_ref().unwrap().write_check(addr, amount).tx;
         self.set_default_params(&mut tx);
         tx
@@ -166,22 +209,33 @@ impl SmallBankTransactionHandler {
 
     fn set_default_params(&self, tx: &mut TypedTransaction) {
         tx.set_from(self.admin_wallet.address())
-        .set_to(H160::from_str(DEFAULT_CONTRACT_ADDRESS).unwrap())
-        .set_chain_id(self.chain_id)
-        .set_nonce(U256::from(std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_nanos()))
-        .set_gas(u64::MAX)
-        .set_gas_price(U256::zero());
+            .set_to(H160::from_str(DEFAULT_CONTRACT_ADDRESS).unwrap())
+            .set_chain_id(self.chain_id)
+            .set_nonce(U256::from(
+                std::time::SystemTime::now()
+                    .duration_since(std::time::UNIX_EPOCH)
+                    .unwrap()
+                    .as_nanos(),
+            ))
+            .set_gas(u64::MAX)
+            .set_gas_price(U256::zero());
     }
 
     fn get_signed(&self, tx: &mut TypedTransaction) -> EthereumTransaction {
-        let signature: Signature = self.admin_wallet.sign_transaction_sync(tx).expect("signature failed");
+        let signature: Signature = self
+            .admin_wallet
+            .sign_transaction_sync(tx)
+            .expect("signature failed");
         let tx_bytes = tx.rlp_signed(&signature).0.to_vec();
         let rlp_signed = tx_bytes.as_slice();
         EthereumTransaction::from_rlp(rlp_signed).unwrap()
     }
 
     fn get_raw_signed(&self, tx: TypedTransaction) -> Bytes {
-        let signature: Signature = self.admin_wallet.sign_transaction_sync(&tx).expect("signature failed");
+        let signature: Signature = self
+            .admin_wallet
+            .sign_transaction_sync(&tx)
+            .expect("signature failed");
         tx.rlp_signed(&signature).0
     }
 }
@@ -194,14 +248,16 @@ impl SmallBankTransactionHandler {
     clippy::upper_case_acronyms,
     clippy::type_complexity,
     dead_code,
-    non_camel_case_types,
+    non_camel_case_types
 )]
 pub mod small_bank {
     #[rustfmt::skip]
     const __ABI: &str = "[{\"constant\":false,\"inputs\":[{\"name\":\"acc\",\"type\":\"string\"},{\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"updateSaving\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"acc\",\"type\":\"string\"},{\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"writeCheck\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"dest\",\"type\":\"string\"},{\"name\":\"src\",\"type\":\"string\"}],\"name\":\"amalgamate\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"acc\",\"type\":\"string\"},{\"name\":\"initCheck\",\"type\":\"uint256\"},{\"name\":\"initSave\",\"type\":\"uint256\"}],\"name\":\"createAccount\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"acc\",\"type\":\"string\"}],\"name\":\"getBalance\",\"outputs\":[{\"name\":\"balance\",\"type\":\"uint256\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"dest\",\"type\":\"string\"},{\"name\":\"src\",\"type\":\"string\"},{\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"sendPayment\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"acc\",\"type\":\"string\"},{\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"depositChecking\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]";
     ///The parsed JSON ABI of the contract.
-    pub static SMALLBANK_ABI: ::ethers::contract::Lazy<::ethers::core::abi::Abi> = ::ethers::contract::Lazy::new(||
-    ::ethers::core::utils::__serde_json::from_str(__ABI).expect("ABI is always valid"));
+    pub static SMALLBANK_ABI: ::ethers::contract::Lazy<::ethers::core::abi::Abi> =
+        ::ethers::contract::Lazy::new(|| {
+            ::ethers::core::utils::__serde_json::from_str(__ABI).expect("ABI is always valid")
+        });
     pub struct SmallBank<M>(::ethers::contract::Contract<M>);
     impl<M> ::core::clone::Clone for SmallBank<M> {
         fn clone(&self) -> Self {
@@ -221,7 +277,9 @@ pub mod small_bank {
     }
     impl<M> ::core::fmt::Debug for SmallBank<M> {
         fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
-            f.debug_tuple(stringify!(SmallBank)).field(&self.address()).finish()
+            f.debug_tuple(stringify!(SmallBank))
+                .field(&self.address())
+                .finish()
         }
     }
     impl<M: ::ethers::providers::Middleware> SmallBank<M> {
@@ -231,13 +289,11 @@ pub mod small_bank {
             address: T,
             client: ::std::sync::Arc<M>,
         ) -> Self {
-            Self(
-                ::ethers::contract::Contract::new(
-                    address.into(),
-                    SMALLBANK_ABI.clone(),
-                    client,
-                ),
-            )
+            Self(::ethers::contract::Contract::new(
+                address.into(),
+                SMALLBANK_ABI.clone(),
+                client,
+            ))
         }
         ///Calls the contract's `amalgamate` (0x26328cdd) function
         pub fn amalgamate(
@@ -311,8 +367,7 @@ pub mod small_bank {
                 .expect("method not found (this should never happen)")
         }
     }
-    impl<M: ::ethers::providers::Middleware> From<::ethers::contract::Contract<M>>
-    for SmallBank<M> {
+    impl<M: ::ethers::providers::Middleware> From<::ethers::contract::Contract<M>> for SmallBank<M> {
         fn from(contract: ::ethers::contract::Contract<M>) -> Self {
             Self::new(contract.address(), contract.client())
         }
@@ -326,7 +381,7 @@ pub mod small_bank {
         Debug,
         PartialEq,
         Eq,
-        Hash
+        Hash,
     )]
     #[ethcall(name = "amalgamate", abi = "amalgamate(string,string)")]
     pub struct AmalgamateCall {
@@ -342,7 +397,7 @@ pub mod small_bank {
         Debug,
         PartialEq,
         Eq,
-        Hash
+        Hash,
     )]
     #[ethcall(name = "createAccount", abi = "createAccount(string,uint256,uint256)")]
     pub struct CreateAccountCall {
@@ -359,7 +414,7 @@ pub mod small_bank {
         Debug,
         PartialEq,
         Eq,
-        Hash
+        Hash,
     )]
     #[ethcall(name = "depositChecking", abi = "depositChecking(string,uint256)")]
     pub struct DepositCheckingCall {
@@ -375,7 +430,7 @@ pub mod small_bank {
         Debug,
         PartialEq,
         Eq,
-        Hash
+        Hash,
     )]
     #[ethcall(name = "getBalance", abi = "getBalance(string)")]
     pub struct GetBalanceCall {
@@ -390,7 +445,7 @@ pub mod small_bank {
         Debug,
         PartialEq,
         Eq,
-        Hash
+        Hash,
     )]
     #[ethcall(name = "sendPayment", abi = "sendPayment(string,string,uint256)")]
     pub struct SendPaymentCall {
@@ -407,7 +462,7 @@ pub mod small_bank {
         Debug,
         PartialEq,
         Eq,
-        Hash
+        Hash,
     )]
     #[ethcall(name = "updateSaving", abi = "updateSaving(string,uint256)")]
     pub struct UpdateSavingCall {
@@ -423,7 +478,7 @@ pub mod small_bank {
         Debug,
         PartialEq,
         Eq,
-        Hash
+        Hash,
     )]
     #[ethcall(name = "writeCheck", abi = "writeCheck(string,uint256)")]
     pub struct WriteCheckCall {
@@ -446,32 +501,29 @@ pub mod small_bank {
             data: impl AsRef<[u8]>,
         ) -> ::core::result::Result<Self, ::ethers::core::abi::AbiError> {
             let data = data.as_ref();
-            if let Ok(decoded)
-                = <AmalgamateCall as ::ethers::core::abi::AbiDecode>::decode(data) {
+            if let Ok(decoded) = <AmalgamateCall as ::ethers::core::abi::AbiDecode>::decode(data) {
                 return Ok(Self::Amalgamate(decoded));
             }
-            if let Ok(decoded)
-                = <CreateAccountCall as ::ethers::core::abi::AbiDecode>::decode(data) {
+            if let Ok(decoded) = <CreateAccountCall as ::ethers::core::abi::AbiDecode>::decode(data)
+            {
                 return Ok(Self::CreateAccount(decoded));
             }
-            if let Ok(decoded)
-                = <DepositCheckingCall as ::ethers::core::abi::AbiDecode>::decode(data) {
+            if let Ok(decoded) =
+                <DepositCheckingCall as ::ethers::core::abi::AbiDecode>::decode(data)
+            {
                 return Ok(Self::DepositChecking(decoded));
             }
-            if let Ok(decoded)
-                = <GetBalanceCall as ::ethers::core::abi::AbiDecode>::decode(data) {
+            if let Ok(decoded) = <GetBalanceCall as ::ethers::core::abi::AbiDecode>::decode(data) {
                 return Ok(Self::GetBalance(decoded));
             }
-            if let Ok(decoded)
-                = <SendPaymentCall as ::ethers::core::abi::AbiDecode>::decode(data) {
+            if let Ok(decoded) = <SendPaymentCall as ::ethers::core::abi::AbiDecode>::decode(data) {
                 return Ok(Self::SendPayment(decoded));
             }
-            if let Ok(decoded)
-                = <UpdateSavingCall as ::ethers::core::abi::AbiDecode>::decode(data) {
+            if let Ok(decoded) = <UpdateSavingCall as ::ethers::core::abi::AbiDecode>::decode(data)
+            {
                 return Ok(Self::UpdateSaving(decoded));
             }
-            if let Ok(decoded)
-                = <WriteCheckCall as ::ethers::core::abi::AbiDecode>::decode(data) {
+            if let Ok(decoded) = <WriteCheckCall as ::ethers::core::abi::AbiDecode>::decode(data) {
                 return Ok(Self::WriteCheck(decoded));
             }
             Err(::ethers::core::abi::Error::InvalidData.into())
@@ -480,27 +532,13 @@ pub mod small_bank {
     impl ::ethers::core::abi::AbiEncode for SmallBankCalls {
         fn encode(self) -> Vec<u8> {
             match self {
-                Self::Amalgamate(element) => {
-                    ::ethers::core::abi::AbiEncode::encode(element)
-                }
-                Self::CreateAccount(element) => {
-                    ::ethers::core::abi::AbiEncode::encode(element)
-                }
-                Self::DepositChecking(element) => {
-                    ::ethers::core::abi::AbiEncode::encode(element)
-                }
-                Self::GetBalance(element) => {
-                    ::ethers::core::abi::AbiEncode::encode(element)
-                }
-                Self::SendPayment(element) => {
-                    ::ethers::core::abi::AbiEncode::encode(element)
-                }
-                Self::UpdateSaving(element) => {
-                    ::ethers::core::abi::AbiEncode::encode(element)
-                }
-                Self::WriteCheck(element) => {
-                    ::ethers::core::abi::AbiEncode::encode(element)
-                }
+                Self::Amalgamate(element) => ::ethers::core::abi::AbiEncode::encode(element),
+                Self::CreateAccount(element) => ::ethers::core::abi::AbiEncode::encode(element),
+                Self::DepositChecking(element) => ::ethers::core::abi::AbiEncode::encode(element),
+                Self::GetBalance(element) => ::ethers::core::abi::AbiEncode::encode(element),
+                Self::SendPayment(element) => ::ethers::core::abi::AbiEncode::encode(element),
+                Self::UpdateSaving(element) => ::ethers::core::abi::AbiEncode::encode(element),
+                Self::WriteCheck(element) => ::ethers::core::abi::AbiEncode::encode(element),
             }
         }
     }
@@ -561,7 +599,7 @@ pub mod small_bank {
         Debug,
         PartialEq,
         Eq,
-        Hash
+        Hash,
     )]
     pub struct GetBalanceReturn {
         pub balance: ::ethers::core::types::U256,

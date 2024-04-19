f26cb29b8f3264fe252a2a647ce8390b061fba69
DongHyeon Ryu <ydh9516@gmail.com>
2024-04-19 13:08:24 +0900

    add zipfian parameter to the benchmark of serial execution

--- crates/sslab-execution/serial/benches/serial.rs
+++ crates/sslab-execution/serial/benches/serial.rs
@@ -33,25 +33,38 @@ fn _create_random_smallbank_workload(
 }
 
 fn serial(c: &mut Criterion) {
+    let s = [0.0, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0];
     let param = 1..81;
     let mut group = c.benchmark_group("Serial");
-    for i in param {
-        group.throughput(Throughput::Elements((DEFAULT_BATCH_SIZE * i) as u64));
-        group.bench_with_input(criterion::BenchmarkId::new("blocksize", i), &i, |b, i| {
-            b.to_async(tokio::runtime::Runtime::new().unwrap())
-                .iter_batched(
-                    || {
-                        let consensus_output =
-                            _create_random_smallbank_workload(0.0, DEFAULT_BATCH_SIZE, *i);
-                        let serial = _get_serial_executor();
-                        (serial, consensus_output)
-                    },
-                    |(serial, consensus_output)| async move {
-                        let _ = serial.execute(consensus_output).await;
-                    },
-                    BatchSize::SmallInput,
-                );
-        });
+    for zipfian in s {
+        for i in param.clone() {
+            group.throughput(Throughput::Elements((DEFAULT_BATCH_SIZE * i) as u64));
+            group.bench_with_input(
+                criterion::BenchmarkId::new(
+                    "blocksize",
+                    format!("(zipfian: {zipfian}, #batch: {i})"),
+                ),
+                &i,
+                |b, i| {
+                    b.to_async(tokio::runtime::Runtime::new().unwrap())
+                        .iter_batched(
+                            || {
+                                let consensus_output = _create_random_smallbank_workload(
+                                    zipfian,
+                                    DEFAULT_BATCH_SIZE,
+                                    *i,
+                                );
+                                let serial = _get_serial_executor();
+                                (serial, consensus_output)
+                            },
+                            |(serial, consensus_output)| async move {
+                                let _ = serial.execute(consensus_output).await;
+                            },
+                            BatchSize::SmallInput,
+                        );
+                },
+            );
+        }
     }
 }
 
